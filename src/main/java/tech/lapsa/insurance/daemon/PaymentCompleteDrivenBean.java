d92aacd300d8fbac0eb9d271d21340351e7eaec3
Vadim.Isaev <vadim.o.isaev@gmail.com>
2017-10-11 19:19:08 +0600

    implemented as a super-class for MessageDrivenBean

--- src/main/java/tech/lapsa/insurance/daemon/PaymentCompleteDrivenBean.java
+++ src/main/java/tech/lapsa/insurance/daemon/PaymentCompleteDrivenBean.java
@@ -9,17 +9,16 @@ import tech.lapsa.insurance.facade.InsuranceRequestFacade;
 import tech.lapsa.javax.jms.ObjectConsumerListener;
 
 @MessageDriven(name = PaymentCompleteDrivenBean.JNDI_JMS_DEST)
-public class PaymentCompleteDrivenBean implements ObjectConsumerListener<Ebill> {
+public class PaymentCompleteDrivenBean extends ObjectConsumerListener<Ebill> {
+
+    public PaymentCompleteDrivenBean() {
+	super(Ebill.class);
+    }
 
     public static final String JNDI_JMS_DEST = EpaymentFacadeBean.JNDI_JMS_DEST_PAID_EBILLs;
 
     @Inject
     private InsuranceRequestFacade facade;
-
-    @Override
-    public Class<Ebill> getObjectClazz() {
-	return Ebill.class;
-    }
 
     @Override
     public void accept(Ebill object) {
