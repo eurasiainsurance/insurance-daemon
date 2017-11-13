62fadec08e1039f7b30ba3e418c1d0e8cb18c10d
Vadim.Isaev <vadim.o.isaev@gmail.com>
2017-11-13 18:54:58 +0600

    general implementation

--- src/main/java/tech/lapsa/insurance/daemon/PaymentCompleteDrivenBean.java
+++ src/main/java/tech/lapsa/insurance/daemon/PaymentCompleteDrivenBean.java
@@ -1,27 +1,43 @@
 package tech.lapsa.insurance.daemon;
 
+import java.time.Instant;
+import java.util.Properties;
+
 import javax.ejb.MessageDriven;
 import javax.inject.Inject;
 
-import tech.lapsa.epayment.facade.Ebill;
+import tech.lapsa.epayment.domain.Invoice;
+import tech.lapsa.epayment.domain.PaymentMethod;
+import tech.lapsa.epayment.domain.QazkomPayment;
 import tech.lapsa.epayment.facade.beans.EpaymentFacadeBean;
 import tech.lapsa.insurance.facade.InsuranceRequestFacade;
+import tech.lapsa.java.commons.function.MyObjects;
 import tech.lapsa.javax.jms.ObjectConsumerListener;
 
 @MessageDriven(mappedName = PaymentCompleteDrivenBean.JNDI_JMS_DEST)
-public class PaymentCompleteDrivenBean extends ObjectConsumerListener<Ebill> {
+public class PaymentCompleteDrivenBean extends ObjectConsumerListener<Invoice> {
 
     public PaymentCompleteDrivenBean() {
-	super(Ebill.class);
+	super(Invoice.class);
     }
 
     public static final String JNDI_JMS_DEST = EpaymentFacadeBean.JNDI_JMS_DEST_PAID_EBILLs;
 
     @Inject
-    private InsuranceRequestFacade facade;
+    private InsuranceRequestFacade insuranceRequests;
 
     @Override
-    public void accept(Ebill object) {
-	facade.markPaymentComplete(Integer.valueOf(object.getExternalId()), object.getReference(), object.getPaid());
+    public void accept(Invoice invoice, Properties properties) {
+	switch (invoice.getPayment().getMethod()) {
+	case QAZKOM:
+	    String methodName = PaymentMethod.QAZKOM.regular();
+	    QazkomPayment qp = MyObjects.requireA(invoice, QazkomPayment.class);
+	    Integer id = Integer.valueOf(invoice.getExternalId());
+	    Instant paid = qp.getCreated();
+	    String ref = qp.getReference();
+	    Double amount = qp.getAmount();
+	    insuranceRequests.markPaymentSuccessful(id, methodName, paid, amount, ref);
+	    break;
+	}
     }
 }
