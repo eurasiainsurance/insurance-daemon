d3e4f0076ac34c15265640c983b6bf890e34d672
Vadim.Isaev <vadim.o.isaev@gmail.com>
2018-01-03 18:47:25 +0600

    compl

--- src/main/java/tech/lapsa/insurance/daemon/drivenBeans/InvoiceHasPaidDrivenBean.java
+++ src/main/java/tech/lapsa/insurance/daemon/drivenBeans/InvoiceHasPaidDrivenBean.java
@@ -1,45 +1,48 @@
 package tech.lapsa.insurance.daemon.drivenBeans;
 
-import static tech.lapsa.java.commons.function.MyExceptions.*;
-
 import java.time.Instant;
 import java.util.Currency;
 import java.util.Properties;
 
+import javax.ejb.EJB;
 import javax.ejb.MessageDriven;
-import javax.inject.Inject;
 
-import tech.lapsa.epayment.shared.entity.XmlInvoiceHasPaidEvent;
+import tech.lapsa.epayment.shared.entity.InvoiceHasPaidJmsEvent;
 import tech.lapsa.epayment.shared.jms.EpaymentDestinations;
-import tech.lapsa.insurance.facade.InsuranceRequestFacade;
-import tech.lapsa.java.commons.function.MyExceptions.IllegalArgument;
-import tech.lapsa.java.commons.function.MyExceptions.IllegalState;
-import tech.lapsa.javax.jms.service.JmsReceiverServiceDrivenBean;
+import tech.lapsa.insurance.facade.InsuranceRequestFacade.InsuranceRequestFacadeRemote;
+import tech.lapsa.java.commons.exceptions.IllegalArgument;
+import tech.lapsa.lapsa.jmsRPC.service.JmsReceiverServiceDrivenBean;
 
 @MessageDriven(mappedName = EpaymentDestinations.INVOICE_HAS_PAID)
-public class InvoiceHasPaidDrivenBean extends JmsReceiverServiceDrivenBean<XmlInvoiceHasPaidEvent> {
+public class InvoiceHasPaidDrivenBean extends JmsReceiverServiceDrivenBean<InvoiceHasPaidJmsEvent> {
 
     public InvoiceHasPaidDrivenBean() {
-	super(XmlInvoiceHasPaidEvent.class);
+	super(InvoiceHasPaidJmsEvent.class);
     }
 
     @Override
-    public void receiving(XmlInvoiceHasPaidEvent entity, Properties properties) {
-	reThrowAsUnchecked(() -> _receiving(entity, properties));
+    public void receiving(final InvoiceHasPaidJmsEvent entity, final Properties properties)
+	    throws IllegalArgumentException, IllegalStateException {
+	_receiving(entity, properties);
     }
 
     // PRIVATE
 
-    @Inject
-    private InsuranceRequestFacade insuranceRequests;
+    @EJB
+    private InsuranceRequestFacadeRemote insuranceRequests;
 
-    private void _receiving(XmlInvoiceHasPaidEvent entity, Properties properties) throws IllegalArgument, IllegalState {
+    private void _receiving(final InvoiceHasPaidJmsEvent entity, final Properties properties)
+	    throws IllegalArgumentException, IllegalStateException {
 	final String methodName = entity.getMethod();
 	final Integer id = Integer.valueOf(entity.getExternalId());
 	final Instant paid = entity.getInstant();
 	final Double amount = entity.getAmount();
 	final Currency currency = entity.getCurrency();
 	final String ref = entity.getReferenceNumber();
-	insuranceRequests.completePayment(id, methodName, paid, amount, currency, ref);
+	try {
+	    insuranceRequests.completePayment(id, methodName, paid, amount, currency, ref);
+	} catch (final IllegalArgument e) {
+	    throw e.getRuntime();
+	}
     }
 }
